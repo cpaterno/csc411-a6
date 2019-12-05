b17ec5285620bef2a6af634f0b6fe0daa3185c77
cpaterno <cpaterno@my.uri.edu>
2019-12-05 16:07:59 -0500

    Inlined instructions and parser modules

--- um_instructions.h
+++ um_instructions.h
@@ -5,39 +5,185 @@
 // all instructions that a Universal Machine can do,
 // excluding HALT
 // All functions in this module modify the UM by reference
+#include <stdio.h>
+#include "assert.h"
+#include "array.h"
 #include "um_rep.h"
+#include "um_instructions.h"
+#include "um_mem.h"
 
-// conditional move overwrites register a with the value in register b if 
+// make sure reg index is in bounds
+static inline void ok_reg(umword r) {
+    assert(r < NUM_REGS);
+}
+
+// conditional move overwrites register a with the value in register b if
 // the value in register c is not 0
-extern void cmov(UM_T um, umword a, umword b, umword c);
-// segmented load overwrites register a with the memory block represented 
+static inline void cmov(UM_T um, umword a, umword b, umword c) {
+    assert(um);
+    ok_reg(a);
+    ok_reg(b);
+    ok_reg(c);
+    if (um->regs[c]) {
+        um->regs[a] = um->regs[b];
+    }
+    // move program counter
+    ++(um->prog_count);    
+}
+
+// segmented load overwrites register a with the memory block represented
 // by the values at register b and register c
-extern void seg_load(UM_T um, umword a, umword b, umword c);
-// segmented store overwrites the memory block  represented by the values 
-// at register a and register b with the value in register c 
-extern void seg_store(UM_T um, umword a, umword b, umword c);
-// add the values in register b and c, put the result in register a 
-extern void add(UM_T um, umword a, umword b, umword c);
+static inline void seg_load(UM_T um, umword a, umword b, umword c) {
+    assert(um);
+    ok_reg(a);
+    ok_reg(b);
+    ok_reg(c);
+    // Array_get handles the 5th Fail State: SegLoad accesses something 
+    // out of bounds of the memory pool or out of bounds of the segment 
+    Array_T seg = (Array_T)Seq_get(um->memory.mem, um->regs[b]);
+    // Array_get also handles the 3rd Fail State: SegLoad refers to unmapped (NULL)
+    umword *e = (umword *)Array_get(seg, um->regs[c]);
+    um->regs[a] = *e;
+    // move program counter
+    ++(um->prog_count);    
+}
+
+// segmented store overwrites the memory block  represented by the values
+// at register a and register b with the value in register c
+static inline void seg_store(UM_T um, umword a, umword b, umword c) {
+    assert(um);
+    ok_reg(a);
+    ok_reg(b);
+    ok_reg(c);
+    // Array_get handles the 6th Fail State: SegStore accesses something 
+    // out of bounds of the memory pool or out of bounds of the segment 
+    Array_T seg = (Array_T)Seq_get(um->memory.mem, um->regs[a]);
+    // Array_get handles the 4th Fail State: SegStore refers to unmapped (NULL)
+    umword *e = (umword *)Array_get(seg, um->regs[b]);
+    *e = um->regs[c];
+    // move program counter
+    ++(um->prog_count);    
+}
+
+// add the values in register b and c, put the result in register a
+static inline void add(UM_T um, umword a, umword b, umword c) {
+    assert(um);
+    ok_reg(a);
+    ok_reg(b);
+    ok_reg(c);
+    um->regs[a] = (um->regs[b] + um->regs[c]);
+    // move program counter
+    ++(um->prog_count);    
+}
+
 // multiply the values in register b and c, put the result in register a
-extern void multiply(UM_T um, umword a, umword b, umword c);
+static inline void multiply(UM_T um, umword a, umword b, umword c) {
+    assert(um);
+    ok_reg(a);
+    ok_reg(b);
+    ok_reg(c);
+    um->regs[a] = (um->regs[b] * um->regs[c]);
+    // move program counter
+    ++(um->prog_count);    
+}
+
 // divide the values in register b and c, put the result in register a
-extern void divide(UM_T um, umword a, umword b, umword c);
-// bitwise not and on the values in register b and c, 
-// put the result in register a 
-extern void nand(UM_T um, umword a, umword b, umword c);
+static inline void divide(UM_T um, umword a, umword b, umword c) {
+    assert(um);
+    ok_reg(a);
+    ok_reg(b);
+    ok_reg(c);
+    // Handle 9th Fail State: Division by 0
+    assert(um->regs[c]);
+    um->regs[a] = (um->regs[b] / um->regs[c]);
+    // move program counter
+    ++(um->prog_count);    
+}
+
+// bitwise not and on the values in register b and c,
+// put the result in register a
+static inline void nand(UM_T um, umword a, umword b, umword c) {
+    assert(um);
+    ok_reg(a);
+    ok_reg(b);
+    ok_reg(c);
+    um->regs[a] = ~((um->regs[b]) & (um->regs[c]));
+    // move program counter
+    ++(um->prog_count);    
+}
+
 // map a new segment of memory with size equal to whats in register c 
 // and store the segment ID in register b
-extern void map(UM_T um, umword b, umword c);
+static inline void map(UM_T um, umword b, umword c) {
+    assert(um);
+    ok_reg(b);
+    ok_reg(c);
+    um->regs[b] = allocate(&(um->memory), um->regs[c]);
+    // move program counter
+    ++(um->prog_count);    
+}
+
 // unmap a segment of memory with its segment ID in register c
-extern void unmap(UM_T um, umword c);
+static inline void unmap(UM_T um, umword c) {
+    assert(um);
+    ok_reg(c);
+    // 7th Fail State: Can't unmap segment 0
+    assert(um->regs[c]);
+    deallocate(&(um->memory), um->regs[c]);
+    // move program counter
+    ++(um->prog_count);    
+}
+
 // output a character to the IO device, based on the value of register c
-extern void output(UM_T um, umword c);
+static inline void output(UM_T um, umword c) {
+    assert(um);
+    ok_reg(c);
+    // Handle 11th Fail State: Print invalid character
+    // Valid characters are [0, 255]
+    assert(um->regs[c] <= UINT8_MAX);
+    putchar(um->regs[c]);
+    // move program counter
+    ++(um->prog_count);    
+}
+
 // input a character, using the IO device, to register c
-extern void input(UM_T um, umword c);
-// load a new program into the 0 segment from the memory segment ID 
-// represented by the value of register b, if that value is 0 then 
-// this is a jump, change the program counter to the value in register c
-extern void load_prog(UM_T um, umword b, umword c);
+static inline void input(UM_T um, umword c) {
+    assert(um);
+    ok_reg(c);
+    um->regs[c] = getchar();
+    // char is between [0, 255] or EOF
+    assert(um->regs[c] <= UINT8_MAX || um->regs[c] == (umword)EOF);
+    // move program counter
+    ++(um->prog_count);    
+}
+
+// load a new program into the 0 segment from the memory segment ID
+// represented by the value of register b, if that value is 0 then
+// this is a jump
+static inline void load_prog(UM_T um, umword b, umword c) {
+    assert(um);
+    ok_reg(b);
+    ok_reg(c);
+    if (um->regs[b]) {
+        // free currently loaded program
+        Array_T prog = (Array_T)Seq_get(um->memory.mem, 0);
+        Array_free(&prog);
+        // get new program to load into segment 0
+        Array_T seg = (Array_T)Seq_get(um->memory.mem, um->regs[b]);
+        // Array_copy handles 10th Fail State: 
+        // Loading an unmapped segment as a program
+        Seq_put(um->memory.mem, 0, Array_copy(seg, Array_length(seg)));
+    }
+    // move the program counter
+    um->prog_count = um->regs[c];
+}
+
 // overwrite the value in register a with value
-extern void load_val(UM_T um, umword a, umword val);
+static inline void load_val(UM_T um, umword a, umword val) {
+    assert(um);
+    ok_reg(a);
+    um->regs[a] = val;
+    // move program counter
+    ++(um->prog_count);    
+}
 #endif
--- um_parser.h
+++ um_parser.h
@@ -5,19 +5,48 @@
 // the important parts from an um instruction
 #include "um_rep.h"
 
+#define OP_WIDTH 4
+#define REG_WIDTH 3
+#define VAL_WIDTH 25
+#define OP_LSB 28
+#define A_3_LSB 6
+#define B_3_LSB 3
+#define C_3_LSB 0
+#define A_OTHER_LSB 25
+#define VAL_LSB 0
+#define WSIZE 64
+
+static inline uint64_t getu(uint64_t word, unsigned width, unsigned lsb) {
+    return ((word << (WSIZE - (width + lsb))) >> (WSIZE - width));
+}
+
 // takes word and extracts opcode
-extern umword opcode(umword word);
+static inline umword opcode(umword word) {
+    return getu(word, OP_WIDTH, OP_LSB);
+}
+
 // takes word, a three register instruction, and extracts register a
-extern umword a_three(umword word);
+static inline umword a_three(umword word) {
+    return getu(word, REG_WIDTH, A_3_LSB);
+}
+
 // takes word, a three register instruction, and extracts register b
-extern umword b_three(umword word);
+static inline umword b_three(umword word) {
+    return getu(word, REG_WIDTH, B_3_LSB);
+}
+
 // takes word, a three register instruction, and extracts register c
-extern umword c_three(umword word);
-// takes word, an ther instruction, and extracts register a
-extern umword a_other(umword word);
+static inline umword c_three(umword word) {
+    return getu(word, REG_WIDTH, C_3_LSB);
+}
+
+// takes word, an other instruction, and extracts register a
+static inline umword a_other(umword word) {
+    return getu(word, REG_WIDTH, A_OTHER_LSB);
+}
+
 // takes word, an other instruction, and extracts value
-extern umword val_other(umword word);
-
-
+static inline umword val_other(umword word) {
+    return getu(word, VAL_WIDTH, VAL_LSB);
+}
 #endif
-
