b459bcb7f149024537d0bcedbd241571f81f51c3
cpaterno <cpaterno@my.uri.edu>
2019-12-06 01:39:09 -0500

    Inlined last modules, removed 1 repeated pointer dereference

--- um_loader.h
+++ um_loader.h
@@ -4,10 +4,46 @@
 // Module that loads an UM program from a file
 // Exports 1 function
 #include <stdio.h>
+#include <string.h>
 #include "um_rep.h"
 #include "arr.h" 
 
 // given a file pointer return an Array_T of instruction words,
 // user will have to free Array_T
-extern umword *loader(FILE *fp);
+static inline umword *loader(FILE *fp) {
+    assert(fp);
+    size_t size = sizeof(umword);
+    // initialize instruction array
+    int len = 0;
+    umword *instruct = arr_new(len);
+    umword *tmp = NULL;
+    umword buf = 0;
+    umword *elem = NULL;
+    int i = 0;
+    // keep reading file, until there is not enough bytes yet for an umword
+    while (fread(&buf, 1, size, fp) == size) {
+        // Array needs to be resized
+        if (i == len) {
+            // C++ vector growth algorithm
+            if (len != 0) {
+                len *= 2;
+            } else {
+                len = 1;
+            }
+            tmp = instruct;
+            instruct = arr_clone(instruct, len);
+            arr_free(tmp);
+        }
+        // put buf into instruct
+        elem = arr_at(instruct, i++);
+        // Specialized bit swap for 32 bit words, courtesy of GCC
+        buf = __builtin_bswap32(buf);
+        memcpy(elem, &buf, size);
+    }
+    // Remove extra space
+    tmp = instruct;
+    instruct = arr_clone(instruct, i);
+    arr_free(tmp);
+    return instruct;
+}
 #endif
--- um_mem.h
+++ um_mem.h
@@ -1,13 +1,49 @@
 #ifndef __UM_MEM__
 #define __UM_MEM__
 
-// This module exports 2 functions which allocate and deallocate 
+// This module exports 2 functions which allocate and deallocate
 // memory in a UM's segmented memory pool
-// All functions in this module modify the pool by reference 
+// All functions in this module modify the pool by reference
+#include <stdlib.h>
+#include "mem.h"
+#include "assert.h"
 #include "um_rep.h"
 
 // allocate a new segment with size and return the new segment's ID
-extern umword allocate(SegMem_T pool, umword size);
+static inline umword allocate(SegMem_T pool, umword size) {
+    //assert(pool);
+    // Handle 12th Fail State: Resource Exhaustion aka maximum number
+    // of segments is 2^(num bits in umword) - 1, if this is true
+    // then we can allocate memory
+    // Must be a checked run-time error 
+    assert((umword)Seq_length(pool->mem) < (umword)-1);
+    umword idx = 0;
+    // no holes case
+    if (Stack_empty(pool->hole_idxs)) {
+        // allocate new segment at the end
+        idx = Seq_length(pool->mem);
+        Seq_addhi(pool->mem, arr_new(size));
+    // holes case
+    } else {
+        // allocate new segment at the latest hole index
+        idx = (uintptr_t)Stack_pop(pool->hole_idxs);
+        Seq_put(pool->mem, idx, arr_new(size)); 
+    }
+    return idx;
+}
+
 // deallocate a segment, at id
-extern void deallocate(SegMem_T pool, umword id);
+static inline void deallocate(SegMem_T pool, umword id) {
+    //assert(pool);
+    umword *seg = (umword *)Seq_get(pool->mem, id);
+    // 8th Fail State: 
+    // attempting to unmap a segment which is not mapped
+    assert(seg);
+    arr_free(seg);
+    // put NULL, our representation of an unmapped segment,
+    // into the Seq at id
+    Seq_put(pool->mem, id, NULL);
+    // add hole index to the stack
+    Stack_push(pool->hole_idxs, (void *)(uintptr_t)id);
+}
 #endif
