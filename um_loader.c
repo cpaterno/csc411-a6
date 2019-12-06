37eeccb3c5d2094faf12e97f69f5fcf299046c76
cpaterno <cpaterno@my.uri.edu>
2019-12-05 20:25:17 -0500

    Used built in 32 bit bit shift

--- um_loader.c
+++ um_loader.c
@@ -4,30 +4,19 @@
 #include "um_rep.h"
 #include "um_loader.h"
 
-// Unfortunately I am on a little endian machine, so this is necessary
-static inline void endian_switch(uint8_t *buf, unsigned len) {
-    assert(buf);
-    uint8_t temp = 0;
-    for (unsigned i = 0; i < len / 2; ++i) {
-        temp = buf[i];
-        buf[i] = buf[len - 1 - i];
-        buf[len - 1 - i] = temp;
-    }
-}
-
 // given a file pointer return an Array_T of instruction words,
 // user will have to free Array_T
 Array_T loader(FILE *fp) {
     assert(fp);
+    size_t size = sizeof(umword);
     // initialize instruction array
-    Array_T instruct = Array_new(0, sizeof(umword));
-    size_t size = Array_size(instruct);
-    int len = Array_length(instruct);
-    uint8_t buf[size];
+    int len = 0;
+    Array_T instruct = Array_new(len, size);
+    umword buf = 0;
     umword *elem = NULL;
     int i = 0;
     // keep reading file, until there is not enough bytes yet for an umword
-    while (fread(buf, 1, size, fp) == size) {
+    while (fread(&buf, 1, size, fp) == size) {
         // Array needs to be resized
         if (i == len) {
             // C++ vector growth algorithm
@@ -40,9 +29,9 @@ Array_T loader(FILE *fp) {
         }
         // put buf into instruct
         elem = (umword *)Array_get(instruct, i++);
-        assert(sizeof(*elem) == size);
-        endian_switch(buf, size);
-        memcpy(elem, buf, size);
+        // Specialized bit swap for 32 bit words, courtesy of GCC
+        buf = __builtin_bswap32(buf);
+        memcpy(elem, &buf, size);
     }
     // Remove extra space
     Array_resize(instruct, i);
