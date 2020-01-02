e206ff4e77d39c32716b9cc91c63026e243e6927
cpaterno <cpaterno@my.uri.edu>
2020-01-02 17:55:59 -0500

    Removed unnecessary files and reorganized some things

--- st.h
+++ st.h
@@ -6,6 +6,9 @@
 #include "mem.h"
 #include "assert.h"
 
+// Module represents our Stack data structure, this is a single header
+// library so all functions are inlined
+
 typedef struct Node {
     struct Node *next;
     uint32_t data;
@@ -15,28 +18,33 @@ typedef struct ST {
     Node head;
 } *ST;
 
-
+// make new Stack
 static inline ST st_new(void) {
     ST st;
     NEW0(st);
     return st;
 }
 
+// free it
 static inline void st_free(ST st) {
     Node tmp = NULL;
+    // free each node
     while (st->head) {
         tmp = st->head;
         st->head = st->head->next;
         FREE(tmp);
     }
+    // free stack struct
     FREE(st);
 }
 
+// determine if stack is empty
 static inline uint32_t st_empty(ST st) {
     assert(st);
     return st->head == NULL;
 }
 
+// push something to stack
 static inline void st_push(ST st, uint32_t i) {
     assert(st);
     Node tmp;
@@ -46,6 +54,7 @@ static inline void st_push(ST st, uint32_t i) {
     st->head = tmp;
 }
 
+// remove something from stack
 static inline uint32_t st_pop(ST st) {
     // side effect of asserting st
     assert(!st_empty(st));
